6c55d0c0cec95fde3963710247dfacaa70541379
Giovana Franciele <giovana.leite@ges.inatel.br>
2025-11-05 17:05:50 -0300

    Exercicio01 de POO

---
+++ PrimitivoCrontrole/src/main/java/Exercicio01.java
@@ -0,0 +1,15 @@
+public class Exercicio01 {
+    static void main() {
+        // Quantidade de lanches consumidos a cada 1 hora
+        int primeiro = 10, segundo = 4, terceiro = 2;
+
+        // Fazendo os calculos
+        int total = primeiro + segundo + terceiro;
+        float media = (float) total / 3;
+
+        // Mostrando o resultado
+        System.out.println("TOTAL: " + total);
+        System.out.println("MEDIA = " + media);
+
+    }
+}
