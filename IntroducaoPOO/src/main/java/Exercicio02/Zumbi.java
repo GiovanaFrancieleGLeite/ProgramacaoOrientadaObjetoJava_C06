da62dd19be19c7c892cf03dc4565cecd49a0125f
Giovana Franciele <giovana.leite@ges.inatel.br>
2025-11-09 19:21:34 -0300

    Enviado o exercicio 02 de introducao de POO

---
+++ IntroducaoPOO/src/main/java/Exercicio02/Zumbi.java
@@ -0,0 +1,18 @@
+package Exercicio02;
+
+public class Zumbi {
+
+    double vida;
+    String nome;
+
+    public double getVida(){
+        return vida;
+    }
+
+    // Método para transferir vida para outro zumbi
+    public void tranfereVida(Zumbi zumbiAlvo, double quantia){
+        this.vida -= quantia;
+        zumbiAlvo.vida += quantia;
+    }
+
+}
